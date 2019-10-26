151ad13bf53e5c840428583101b5391307b5d120
Andrey <100stepstosuccess@gmail.com>
2019-10-26 23:17:16 +0300

    initial

---
+++ src/pages/palitras.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Palitras = () => {
+  return (
+    <Layout>
+      <SEO title="Palitras" />
+      <h1>Palitras</h1>
+      <p>more about palitras</p>
+    </Layout>
+  )
+}
+
+export default Palitras
